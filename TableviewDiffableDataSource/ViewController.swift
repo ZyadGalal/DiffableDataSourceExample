81b1e6591ac48456eda1722bb538a37d3cb9a41a
Zyad_Galal <zyad_mg@yahoo.com>
2021-01-03 13:21:15 +0200

    inital commit

--- TableviewDiffableDataSource/ViewController.swift
+++ TableviewDiffableDataSource/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var dataSource: UITableViewDiffableDataSource<Int, Student>?
+    var snapShot = NSDiffableDataSourceSnapshot<Int, Student>()
+    var students: [Student] = [Student(id: UUID(), name: "zyad") , Student(id: UUID(), name: "mahmoud") , Student(id: UUID(), name: "galal el-din") , Student(id: UUID(), name: "soliman")]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        dataSource = UITableViewDiffableDataSource<Int , Student>(tableView: tableView) { (table, index, student) -> UITableViewCell? in
+            let cell = table.dequeueReusableCell(withIdentifier: "cell")!
+            cell.textLabel?.text = student.name
+            cell.detailTextLabel?.text = "\(student.id)"
+            return cell
+        }
+        tableView.dataSource = dataSource
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+
+        snapShot.appendSections([0])
+        
+        snapShot.appendItems(students, toSection: 0)
+        dataSource?.apply(snapShot)
     }
 
-
+    @IBAction func addButtonClicked(_ sender: Any) {
+        students.append(Student(id: UUID(), name: "zozz"))
+        snapShot.appendItems(students)
+        dataSource?.apply(snapShot)
+    }
 }
-
+extension ViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        snapShot.deleteItems([students[indexPath.row]])
+        students.remove(at: indexPath.row)
+        dataSource?.apply(snapShot)
+    }
+}
+struct Student: Hashable {
+    let id: UUID
+    let name: String
+}
